ccce0bb2678c5bae67c4bbaada4e8821abd45635
Isaac <isaachkdeveloper@gmail.com>
2022-10-19 20:06:47 +0800

    add grpc proto

---
+++ src/hero/hero.controller.ts
@@ -0,0 +1,21 @@
+import { Metadata, ServerUnaryCall } from '@grpc/grpc-js';
+import { Controller } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+import { Hero } from 'src/proto/hero/Hero';
+import { HeroById } from 'src/proto/hero/HeroById';
+
+@Controller()
+export class HeroController {
+  @GrpcMethod('HeroService', 'FindOne')
+  findOne(
+    data: HeroById,
+    metadata: Metadata,
+    call: ServerUnaryCall<any, any>,
+  ): Hero {
+    const items = [
+      { id: 1, name: 'John' },
+      { id: 2, name: 'Doe' },
+    ];
+    return items.find(({ id }) => id === data.id);
+  }
+}
